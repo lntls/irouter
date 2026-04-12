daf71d0fc0c1dfc490d956abf8ceb08f8ca2632b
lntls <lntls@outlook.com>
2026-04-12 23:41:44 +0800

    first commit

---
+++ lib/src/route.dart
@@ -0,0 +1,88 @@
+part of 'router.dart';
+
+final class IPageBuilder {
+  IPageBuilder.custom(this._creator);
+
+  final Route<Object?> Function(BuildContext, Page<Object?>, Widget) _creator;
+
+  @internal
+  Route<Object?> createRoute(
+    BuildContext context,
+    Page<Object?> page,
+    Widget content,
+  ) {
+    return _creator(context, page, content);
+  }
+}
+
+final class IRouteEntry<R, A> {
+  const IRouteEntry._(this._route, this._args);
+
+  final IRoute<R, A> _route;
+
+  final A _args;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is IRouteEntry && _route == other._route && _args == other._args;
+
+  @override
+  int get hashCode => Object.hash(_route, _args);
+}
+
+final class IRoute<R, A> {
+  IRoute._({
+    required Widget Function(BuildContext, A) widgetBuilder,
+    IPageBuilder? routeBuilder,
+  }) : _widgetBuilder = widgetBuilder,
+       _routeBuilder = routeBuilder;
+
+  final IPageBuilder? _routeBuilder;
+
+  final Widget Function(BuildContext, A) _widgetBuilder;
+}
+
+extension IRouteWithArgsExtension<R, A extends Object?> on IRoute<R, A> {
+  IRouteEntry<R, A> call(A args) => IRouteEntry._(this, args);
+}
+
+extension IRouteWithoutArgsExtension<R> on IRoute<R, Never> {
+  IRouteEntry<R, void> call() => IRouteEntry._(this, null);
+}
+
+final class IRouteBuilder<R> {
+  IRouteBuilder._({required this.routeBuilder});
+
+  IPageBuilder? routeBuilder;
+
+  IRouteBuilder<T> withResult<T>() {
+    return IRouteBuilder._(routeBuilder: routeBuilder);
+  }
+}
+
+extension IRouteBuilderWithArgsExtension<R> on IRouteBuilder<R> {
+  IRoute<R, Never> build(Widget Function(BuildContext) builder) {
+    return IRoute._(
+      routeBuilder: routeBuilder,
+      widgetBuilder: (context, _) => builder(context),
+    );
+  }
+
+  IRoute<R, A> buildWith<A extends Object?>(
+    Widget Function(BuildContext, A) builder,
+  ) {
+    return IRoute._(
+      routeBuilder: routeBuilder,
+      widgetBuilder: (context, args) => builder(context, args),
+    );
+  }
+}
+
+final class IRouteFactory {
+  IRouteFactory._();
+
+  IRouteBuilder<void> call<R, A>({IPageBuilder? routeBuilder}) {
+    return IRouteBuilder._(routeBuilder: routeBuilder);
+  }
+}
---
+++ lib/src/router.dart
@@ -0,0 +1,195 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+part 'route.dart';
+
+abstract interface class IRoutingController<T extends Record> {
+  RouterConfig<Object> get config;
+
+  Future<R> push<R, A>(IRouteEntry<R, A> Function(T) selector);
+
+  bool get canPop;
+
+  void pop();
+
+  void dispose();
+}
+
+class _IRouteInformationParser
+    extends RouteInformationParser<RouteInformation> {
+  @override
+  Future<RouteInformation> parseRouteInformation(
+    RouteInformation routeInformation,
+  ) {
+    return SynchronousFuture(routeInformation);
+  }
+}
+
+class _IPage<R, A> extends Page<Object?> {
+  _IPage({required this.routeBuilder, required this.entry, super.onPopInvoked})
+    : super(key: ValueKey(entry));
+
+  final IPageBuilder routeBuilder;
+  final IRouteEntry<R, A> entry;
+
+  @override
+  Route<Object?> createRoute(BuildContext context) {
+    final content = entry._route._widgetBuilder(context, entry._args);
+    final routeBuilder = entry._route._routeBuilder ?? this.routeBuilder;
+    return routeBuilder.createRoute(context, this, content);
+  }
+}
+
+class _InheritedRoutingController<T extends Record> extends InheritedWidget {
+  const _InheritedRoutingController({
+    super.key,
+    required super.child,
+    required this.controller,
+  });
+
+  final IRoutingController<T> controller;
+
+  @override
+  bool updateShouldNotify(_InheritedRoutingController oldWidget) {
+    return oldWidget.controller != controller;
+  }
+}
+
+class _IRouterDelegate<T extends Record>
+    extends RouterDelegate<RouteInformation>
+    with ChangeNotifier
+    implements IRoutingController<T> {
+  _IRouterDelegate({
+    required T Function(IRouteFactory) routes,
+    required this.defaultRouteBuilder,
+    required List<IRouteEntry<Object?, Object?>> Function(T) initialRoutes,
+  }) : _definedRoutes = routes(IRouteFactory._()) {
+    _pages = initialRoutes(_definedRoutes)
+        .map((entry) => _IPage(routeBuilder: defaultRouteBuilder, entry: entry))
+        .toList();
+  }
+
+  final IPageBuilder defaultRouteBuilder;
+
+  final T _definedRoutes;
+
+  late List<Page<Object?>> _pages;
+
+  void _onDidRemovePage(Page<Object?> page) {
+    _pages.remove(page);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _InheritedRoutingController(
+      controller: this,
+      child: Navigator(pages: _pages, onDidRemovePage: _onDidRemovePage),
+    );
+  }
+
+  @override
+  Future<R> push<R, A>(IRouteEntry<R, A> Function(T) selector) async {
+    final resultCompleter = Completer<R>();
+    final entry = selector(_definedRoutes);
+    final newPage = _IPage(
+      routeBuilder: defaultRouteBuilder,
+      entry: entry,
+      onPopInvoked: (didPop, result) {
+        if (didPop) {
+          resultCompleter.complete(result as R);
+        }
+      },
+    );
+    _pages.removeWhere((page) => page.key == newPage.key);
+    _pages = [..._pages, newPage];
+    notifyListeners();
+    return resultCompleter.future;
+  }
+
+  @override
+  bool get canPop => _pages.length > 1;
+
+  void _pop() {
+    _pages = _pages.sublist(0, _pages.length - 1);
+    _pages.removeLast();
+    notifyListeners();
+  }
+
+  @override
+  void pop() {
+    if (canPop) {
+      _pop();
+    }
+  }
+
+  @override
+  Future<bool> popRoute() {
+    if (!canPop) {
+      return SynchronousFuture(false);
+    }
+    _pages = _pages.sublist(0, _pages.length - 1);
+    _pages.removeLast();
+    notifyListeners();
+    return SynchronousFuture(true);
+  }
+
+  @override
+  Future<void> setNewRoutePath(RouteInformation configuration) {
+    return SynchronousFuture(null);
+  }
+
+  @override
+  late final RouterConfig<Object> config = RouterConfig<RouteInformation>(
+    routerDelegate: this,
+    routeInformationProvider: PlatformRouteInformationProvider(
+      initialRouteInformation: RouteInformation(uri: Uri.parse('/')),
+    ),
+    routeInformationParser: _IRouteInformationParser(),
+  );
+}
+
+abstract base class IRouter<T extends Record> {
+  IRouter._();
+
+  factory IRouter({
+    required IPageBuilder routeBuilder,
+    required T Function(IRouteFactory) routes,
+    required List<IRouteEntry<Object?, Object?>> Function(T) initialRoutes,
+  }) = _IRouter;
+
+  IRoutingController<T> createRoutingController();
+
+  IRoutingController<T> of(BuildContext context);
+}
+
+final class _IRouter<T extends Record> extends IRouter<T> {
+  _IRouter({
+    required this.routes,
+    required this.routeBuilder,
+    required this.initialRoutes,
+  }) : super._();
+
+  final IPageBuilder routeBuilder;
+
+  final T Function(IRouteFactory) routes;
+
+  final List<IRouteEntry<Object?, Object?>> Function(T) initialRoutes;
+
+  @override
+  IRoutingController<T> of(BuildContext context) {
+    final widget = context
+        .getInheritedWidgetOfExactType<_InheritedRoutingController<T>>();
+    return widget!.controller;
+  }
+
+  @override
+  IRoutingController<T> createRoutingController() {
+    return _IRouterDelegate(
+      routes: routes,
+      defaultRouteBuilder: routeBuilder,
+      initialRoutes: initialRoutes,
+    );
+  }
+}
